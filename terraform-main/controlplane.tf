43951702c06cf84cee4c1368b03974b2a2bda5ad
Mikhail Baykov <contact@vxoda.net>
2025-12-27 15:58:27 +0300

    добавил outputs.tf; для удобства разнес создание контрольных нод и воркеров из nodes.tf в controlplane.tf и worker.tf

--- terraform-main/controlplane.tf
+++ terraform-main/controlplane.tf
@@ -92,42 +92,42 @@ resource "yandex_compute_instance" "controlplane" {
 #  }
 #}
 
-resource "yandex_compute_instance" "worker" {
-  depends_on                = [yandex_compute_instance.controlplane]
-  count                     = var.worker_count
-  allow_stopping_for_update = true
-  name                      = "worker-${count.index + 1}"
-  platform_id               = var.worker_resources.platform_id
-  zone                      = element(var.zones, count.index % length(var.zones))
-  #  zone = var.zone${count.index + 1}
-  resources {
-    cores         = var.worker_resources.cpu
-    memory        = var.worker_resources.ram
-    core_fraction = var.worker_resources.core_fraction
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.debian12.image_id
-      size     = var.worker_resources.disk
-    }
-  }
-
-  #  metadata = {
-  #    ssh-keys           = "ubuntu:${local.ssh-keys}"
-  #    serial-port-enable = "1"
-  #    user-data          = data.template_file.cloudinit.rendered
-  #  }
-
-  network_interface {
-  #  subnet_id = "yandex_vpc_subnet.element(var.zones, count.index % length(var.zones)).id"
-  #  subnet_id = element(var.subnets, count.index % length(var.subnets))
-    subnet_id = local.subnet_ids[count.index]
-    nat       = true
-
-  }
-
-  scheduling_policy {
-    preemptible = true
-  }
-}
+#resource "yandex_compute_instance" "worker" {
+#  depends_on                = [yandex_compute_instance.controlplane]
+#  count                     = var.worker_count
+#  allow_stopping_for_update = true
+#  name                      = "worker-${count.index + 1}"
+#  platform_id               = var.worker_resources.platform_id
+#  zone                      = element(var.zones, count.index % length(var.zones))
+#  #  zone = var.zone${count.index + 1}
+#  resources {
+#    cores         = var.worker_resources.cpu
+#    memory        = var.worker_resources.ram
+#    core_fraction = var.worker_resources.core_fraction
+#  }
+#
+#  boot_disk {
+#    initialize_params {
+#      image_id = data.yandex_compute_image.debian12.image_id
+#      size     = var.worker_resources.disk
+#    }
+#  }
+#
+#  #  metadata = {
+#  #    ssh-keys           = "ubuntu:${local.ssh-keys}"
+#  #    serial-port-enable = "1"
+#  #    user-data          = data.template_file.cloudinit.rendered
+#  #  }
+#
+#  network_interface {
+#  #  subnet_id = "yandex_vpc_subnet.element(var.zones, count.index % length(var.zones)).id"
+#  #  subnet_id = element(var.subnets, count.index % length(var.subnets))
+#    subnet_id = local.subnet_ids[count.index]
+#    nat       = true
+#
+#  }
+#
+#  scheduling_policy {
+#    preemptible = true
+#  }
+#}
