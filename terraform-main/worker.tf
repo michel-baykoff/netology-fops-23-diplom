7409065bf7e8c4f55ed2b3f5f0af603ba1b2aada
michel-baykoff <150228165+michel-baykoff@users.noreply.github.com>
2025-12-28 17:05:44 +0300

    Update worker.tf

--- terraform-main/worker.tf
+++ terraform-main/worker.tf
@@ -20,7 +20,7 @@ resource "yandex_compute_instance" "worker" {
   }
 
     metadata = {
-      ssh-keys           = "debian:${local.ssh-public-key}"
+      ssh-keys           = "debian:${local.ssh_public_key}"
       serial-port-enable = "1"
 #      user-data = templatefile(
 #      "${path.module}/templates/cloud-config.tftpl",
